7cd86ff2f015baedadfd47c35083579cb9da2d68
Thiel <gabrieln.souza@outlook.com>
2019-01-31 10:19:14 -0200

    imported components in module.exports

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import Button from './src/basics/Button';
 import Text from './src/basics/Text';
+import Card from './src/basics/Card';
+import Icon from './src/basics/Icon';
+import SectionList from './src/basics/SectionList';
+import Avatar from './src/basics/Avatar';
+import Row from './src/basics/Row';
+import Header from './src/basics/Header';
 
 module.exports = {
   Button,
   Text,
+	Card,
+	Icon,
+	SectionList,
+	Avatar,
+	Row,
+	Header,
 };
